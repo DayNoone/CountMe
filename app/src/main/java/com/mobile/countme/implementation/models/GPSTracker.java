f46e1d35c15202e86fa0b5f7a787e68afe2fdb06
torgeiws <torgeiws@stud.ntnu.no>
2015-10-07 11:34:34 +0200

    fix indent

--- app/src/main/java/com/mobile/countme/implementation/models/GPSTracker.java
+++ app/src/main/java/com/mobile/countme/implementation/models/GPSTracker.java
@@ -134,94 +134,94 @@ public class GPSTracker extends Service implements LocationListener {
                 }
             }
             locationManager.removeUpdates(GPSTracker.this);
+        }
+    }
+
+    /**
+     * Function to get latitude
+     * */
+    public double getLatitude(){
+        if(location != null){
+            latitude = location.getLatitude();
+        }
+
+        // return latitude
+        return latitude;
+    }
+
+    /**
+     * Function to get longitude
+     * */
+    public double getLongitude(){
+        if(location != null){
+            longitude = location.getLongitude();
+        }
+
+        // return longitude
+        return longitude;
+    }
+
+    /**
+     * Function to check GPS/wifi enabled
+     * @return boolean
+     * */
+    public boolean canGetLocation() {
+        return this.canGetLocation;
+    }
+
+    /**
+     * Function to show settings alert dialog
+     * On pressing Settings button will lauch Settings Options
+     * */
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
             }
-        }
+        });
 
-        /**
-         * Function to get latitude
-         * */
-        public double getLatitude(){
-            if(location != null){
-                latitude = location.getLatitude();
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
             }
+        });
 
-            // return latitude
-            return latitude;
-        }
+        // Showing Alert Message
+        alertDialog.show();
+    }
 
-        /**
-         * Function to get longitude
-         * */
-        public double getLongitude(){
-            if(location != null){
-                longitude = location.getLongitude();
-            }
+    @Override
+    public void onLocationChanged(Location location) {
+        trip.add(location);
+    }
 
-            // return longitude
-            return longitude;
-        }
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
 
-        /**
-         * Function to check GPS/wifi enabled
-         * @return boolean
-         * */
-        public boolean canGetLocation() {
-            return this.canGetLocation;
-        }
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
 
-        /**
-         * Function to show settings alert dialog
-         * On pressing Settings button will lauch Settings Options
-         * */
-        public void showSettingsAlert(){
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
 
-            // Setting Dialog Title
-            alertDialog.setTitle("GPS is settings");
-
-            // Setting Dialog Message
-            alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
-
-            // On pressing Settings button
-            alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog,int which) {
-                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    mContext.startActivity(intent);
-                }
-            });
-
-            // on pressing cancel button
-            alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.cancel();
-                }
-            });
-
-            // Showing Alert Message
-            alertDialog.show();
-        }
-
-        @Override
-        public void onLocationChanged(Location location) {
-            trip.add(location);
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-        }
-
-        @Override
-        public IBinder onBind(Intent arg0) {
-            return null;
-        }
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
 
     public ArrayList<Location> getTrip(){
         return trip;
