8c36fe62e5178ecea15ba9126eff7d5916dfeb07
perrr <perod@stud.ntnu.no>
2015-10-29 14:04:30 +0100

    Send push notification when trip is stopped

--- app/src/main/java/com/mobile/countme/implementation/views/BikingActive.java
+++ app/src/main/java/com/mobile/countme/implementation/views/BikingActive.java
@@ -1,10 +1,15 @@
 package com.mobile.countme.implementation.views;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.app.AlertDialog;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.Toast;
 
@@ -55,6 +60,37 @@ public class BikingActive extends AppMenu {
         getMainController().addStatistics(getMainController().getTracker().getDistance());
         getMainController().stoptimertask();
         goTo(ResultMenu.class);
+    }
+
+    public void endTripNotification() {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.notification)
+                        .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
+                        .setContentTitle(getString(R.string.stopped_automatically))
+                        .setContentText(getString(R.string.stopped_automatically_long));
+// Creates an explicit intent for an Activity in your app
+        //Intent resultIntent = new Intent(this, ResultActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        //TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        //stackBuilder.addParentStack(ResultActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        //stackBuilder.addNextIntent(resultIntent);
+        //PendingIntent resultPendingIntent =
+        //        stackBuilder.getPendingIntent(
+         //               0,
+          //              PendingIntent.FLAG_UPDATE_CURRENT
+           //     );
+        //mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(0, mBuilder.build());
     }
 
     /**
@@ -116,7 +152,7 @@ public class BikingActive extends AppMenu {
     public void updateView(String time_used, boolean start_using_tracker) {
         getMainController().getTracker().checkIfNoLocationsReceived();
         if (getMainController().getTracker().isAutomaticallyStopped()) {
-            //TODO We could add a Push notification here
+            endTripNotification();
             endTripAndReturn();
         }
         CustomTextView time = (CustomTextView) findViewById(R.id.tracking_time);
